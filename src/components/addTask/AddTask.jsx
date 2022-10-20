b556e2cef10ec45da1792df135336dda860d650c
ozkankomu <ozkankomu35@gmail.com>
2022-10-20 21:56:03 +0300

    ""

--- src/components/addTask/AddTask.jsx
+++ src/components/addTask/AddTask.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
 
 const AddTask = () => {
-  return (
-    <div>AddTask</div>
-  )
-}
+  const [task, setTask] = useState("");
+  const [date, setDate] = useState("");
 
-export default AddTask
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTask = { task, date };
+    console.log(newTask);
+  };
+
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Task</Form.Label>
+          <Form.Control
+            type="text"
+            required
+            placeholder="Enter Task"
+            onChange={(e) => setTask(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Date</Form.Label>
+          <Form.Control
+            required
+            type="date"
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </Form.Group>
+
+        <Button variant="danger btn-m w-50" type="submit">
+          Save
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default AddTask;
